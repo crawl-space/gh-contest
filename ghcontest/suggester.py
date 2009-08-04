2a04540bd685269c40c54e8934ee2e34a2a75870
James Bowes <jbowes@repl.ca>
2009-08-04 14:36:24 -0400

    Try weighting everything equally

--- ghcontest/suggester.py
+++ ghcontest/suggester.py
@@ -36,8 +36,8 @@ class Suggestions(object):
     def __len__(self):
         return len(self.suggested_repos)
 
-PARENT = 5
-ANCESTOR = 4
+PARENT = 2
+ANCESTOR = 2
 USER = 2
 CHILD = 2
 SUPERPROJECT = 1
